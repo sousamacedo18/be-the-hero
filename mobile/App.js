9f5a535cd129bcd09ebd1a06891a3c252904b79e
sousamacedo18 <sousamacedo18@gmail.com>
2020-04-01 21:45:06 -0300

    aplicativo mobile

---
+++ mobile/App.js
@@ -0,0 +1,15 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes';
+import { NavigationContainer } from '@react-navigation/native';
+
+export default function App() {
+  return (
+        <Routes />
+  );
+}
+
+
